60196555c2d7fd35baa14363898b1c3fc1dc8c91
Sakiyary <sakiyary19@qq.com>
2021-12-11 00:11:52 +0800

    almost finish the change between main ui and play ui

--- 8192.c
+++ 8192.c
@@ -9,6 +9,7 @@
 //void MoveRIGHT();
 //
 //void MoveLEFT();
+void LoadStart();
 
 int Map[4][4] = {
         {0, 0, 0, 0},
@@ -20,37 +21,54 @@ int Map[4][4] = {
 SDL_Window *Window = NULL;
 SDL_Renderer *Renderer = NULL;
 
+SDL_Surface *MainBackGroundSurface = NULL;
+SDL_Texture *MainBackGroundTexture = NULL;
+
+SDL_Surface *PlayBackGroundSurface = NULL;
+SDL_Texture *PlayBackGroundTexture = NULL;
 
 int main(int argc, char *argv[]) {
-    Window = SDL_CreateWindow("2048", SDL_WINDOWPOS_UNDEFINED_DISPLAY(1), SDL_WINDOWPOS_UNDEFINED_DISPLAY(1), 800, 1000, SDL_WINDOW_SHOWN);
+    Window = SDL_CreateWindow("Sakiyary's Infinite 2048 ", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 1000, SDL_WINDOW_SHOWN);
     Renderer = SDL_CreateRenderer(Window, -1, SDL_RENDERER_ACCELERATED);
     SDL_Event PlayEvent;
     SDL_Event MainEvent;
+    LoadStart();
     MAIN:
     printf("MainEvent\n");
-    while (SDL_WaitEvent(&MainEvent)) {
+    while (SDL_WaitEvent(&MainEvent)||SDL_PollEvent(&MainEvent)) {
+        SDL_RenderClear(Renderer);
+        SDL_RenderCopy(Renderer, MainBackGroundTexture, NULL, NULL);
+        SDL_RenderPresent(Renderer);
         switch (MainEvent.type) {
             case SDL_QUIT:
                 goto END;
             case SDL_MOUSEBUTTONDOWN:
                 printf("(%d,%d)\n", MainEvent.button.x, MainEvent.button.y);
-                if (MainEvent.button.x > 600 && MainEvent.button.x < 700)
+                if (MainEvent.button.x > 212 && MainEvent.button.x < 604 && MainEvent.button.y > 656 && MainEvent.button.y < 772)
                     goto PLAY;
                 break;
             case SDL_KEYDOWN:
                 switch (MainEvent.key.keysym.sym) {
                     case SDLK_ESCAPE:
                         goto END;
+                    case SDLK_BACKSPACE:
+                        goto END;
+                    case SDLK_RETURN:
+                        goto PLAY;
                     default:
                         break;
                 }
             default:
                 break;
         }
+        SDL_Delay(10);
     }
     PLAY:
     printf("PlayEvent\n");
-    while (SDL_WaitEvent(&PlayEvent)) {
+    while (SDL_WaitEvent(&PlayEvent)||SDL_PollEvent(&PlayEvent)) {
+        SDL_RenderClear(Renderer);
+        SDL_RenderCopy(Renderer, PlayBackGroundTexture, NULL, NULL);
+        SDL_RenderPresent(Renderer);
         switch (PlayEvent.type) {
             case SDL_QUIT:
                 goto END;
@@ -58,6 +76,14 @@ int main(int argc, char *argv[]) {
                 switch (PlayEvent.key.keysym.sym) {
                     case SDLK_ESCAPE:
                         goto MAIN;
+                    case SDLK_BACKSPACE:
+                        goto MAIN;
+                    case SDLK_z:
+                        printf("REVOKE\n");
+                        break;
+                    case SDLK_r:
+                        printf("RESTART\n");
+                        break;
                     case SDLK_UP:
 //                        MoveUP();
                         printf("UP\n");
@@ -70,7 +96,6 @@ int main(int argc, char *argv[]) {
 //                        MoveRIGHT();
                         printf("RIGHT\n");
                         break;
-
                     case SDLK_LEFT:
 //                        MoveLEFT();
                         printf("LEFT\n");
@@ -87,10 +112,24 @@ int main(int argc, char *argv[]) {
         }
         SDL_Delay(10);
     }
-    
+
     END:
+
+    SDL_FreeSurface(MainBackGroundSurface);
+    SDL_FreeSurface(PlayBackGroundSurface);
+
+    SDL_DestroyTexture(MainBackGroundTexture);
+    SDL_DestroyTexture(PlayBackGroundTexture);
+
     SDL_DestroyRenderer(Renderer);
     SDL_DestroyWindow(Window);
+
     SDL_Quit();
 }
 
+void LoadStart() {
+    MainBackGroundSurface = IMG_Load("IMAGE/MainBackGround.png");
+    MainBackGroundTexture = SDL_CreateTextureFromSurface(Renderer, MainBackGroundSurface);
+    PlayBackGroundSurface = IMG_Load("IMAGE/PlayBackGround.png");
+    PlayBackGroundTexture = SDL_CreateTextureFromSurface(Renderer, PlayBackGroundSurface);
+}
